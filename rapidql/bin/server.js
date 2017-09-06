02d8353c3df8f5cae16824371152044007bf98ed
davidnoah <david.t.noah@gmail.com>
2017-09-05 18:12:17 -0700

    removed console logs

--- rapidql/bin/server.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-/**
- * Created by iddo on 7/20/17.
- */
-console.log("server");
--- rapidql/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Iddo on 12/17/2016.
- */
-
-"use strict";
-
-const RQL = require('./src/RQL');
-
-module.exports = RQL;
